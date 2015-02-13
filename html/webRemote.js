76c173a40ba52f56b051dbbbc8f293df83ca3642
Ezra Eibergen <ezeibergen@yahoo.commit>
2015-02-13 17:15:49 -0500

    Cleaning out some exsmple code

--- html/webRemote.js
+++ html/webRemote.js
@@ -1,33 +1,8 @@
 webiopi().ready(function() {
-        // Following function will process data received from set/getLightHours macro.
-        var updateLightHours = function(macro, args, response) {
-            var hours = response.split(";");
-            // Following lines use jQuery functions
-            $("#inputOn").val(hours[0]);
-            $("#inputOff").val(hours[1]);
-        }
-
-        // Immediately call getLightHours macro to update the UI with current values
-        // "getLightHours" refers to macro name
-        // [] is an empty array, because getLightHours macro does not take any argument
-        // updateLightHours is the callback function, defined above
-        webiopi().callMacro("getLightHours", [], updateLightHours);
-        
-        // Create a button to call setLightHours macro
-        var sendButton = webiopi().createButton("sendButton", "Send", function() {
-           // Arguments sent to the macro
-            var hours = [$("#inputOn").val(), $("#inputOff").val()];
-            // Call the macro
-            webiopi().callMacro("setLightHours", hours, updateLightHours);
-        });
-
-        // Append the button to the controls box using a jQuery function
-        $("#controls").append(sendButton);
-
 				//Create a power button to call the tvPower macro
 				var powerButton = webiopi().createButton("powerButton", "Power", function() {
 				    //Call Macro
-				    webiopi().callMacro("tvPower", [], updateLightHours);
+				    webiopi().callMacro("tvPower", []);
 				});
 
 				// Append the button to the controls box
@@ -36,7 +11,7 @@ webiopi().ready(function() {
 				//Create a power button to call the tvVolumeUp macro
 				var volUpButton = webiopi().createButton("volUpButton", "VolUp", function() {
 				    //Call Macro
-				    webiopi().callMacro("tvVolumeUp", [], updateLightHours);
+				    webiopi().callMacro("tvVolumeUp");
 				});
 
 				// Append the button to the controls box
